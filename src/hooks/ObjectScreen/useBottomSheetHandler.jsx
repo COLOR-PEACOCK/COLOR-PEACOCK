4a1ddb37d3cfda174003d8edb19df4fac692b11c
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-10-17 01:44:22 +0900

    Refactor: ColorBottomSheet 리팩토링 - CustomHandler 컴포넌트 분리 - 컬러바텀시트 함수 훅으로 분리

---
+++ src/hooks/ObjectScreen/useBottomSheetHandler.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+import { useObjectState } from '@hooks/ObjectScreen/objectStateContext';
+
+const useBottomSheetHandler = bottomSheetRef => {
+	const {
+		selectedItemId,
+		setDroppedItems,
+		isColorPickerOpen,
+		setIsColorPickerOpen,
+	} = useObjectState();
+
+	//컬러 팔레트 터치 이벤트
+	const handleColorSelect = color => {
+		if (selectedItemId) {
+			setDroppedItems(prevItems =>
+				prevItems.map(item =>
+					item.id === selectedItemId
+						? { ...item, color: color }
+						: item,
+				),
+			);
+		}
+	};
+	useEffect(() => {
+		isColorPickerOpen
+			? bottomSheetRef?.current?.snapToIndex(1)
+			: bottomSheetRef?.current?.snapToIndex(0);
+	}, [isColorPickerOpen]);
+
+	// 커스텀 핸들러 터치 이벤트
+	const handlerTouchEvent = () => {
+		setIsColorPickerOpen(!isColorPickerOpen);
+	};
+
+	return { handleColorSelect, handlerTouchEvent };
+};
+export default useBottomSheetHandler;
