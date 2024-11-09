f849d076d22ee31ca9fc101dd7a307d21a0518e7
xogus7 <xogus2676@gmail.com>
2024-11-10 01:42:51 +0900

    Rename: hooks Home, utils Home

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,7 @@
 import useColorName from '@hooks/useColorName';
 import useGemini from '@hooks/useGemini';
-import useImageWebview from '@hooks/useImageWebview';
 import useModal from '@hooks/useModal';
 import useImagePicker from '@hooks/useImagePicker';
 import useCamera from '@hooks/useCamera';
 
-export { useColorName, useGemini, useImageWebview, useModal, useImagePicker, useCamera };
+export { useColorName, useGemini, useModal, useImagePicker, useCamera };
--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const useModal = () => {
-	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
 	const handleCloseModal = () => {
 		setIsModalVisible(false);
