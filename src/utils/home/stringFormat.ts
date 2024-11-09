fc74fdf27797ee2273613a6cad76568faa4fffc6
xogus7 <70815654+xogus7@users.noreply.github.com>
2024-11-10 01:44:28 +0900

    Remove: Delete src/utils/home directory

--- src/utils/home/stringFormat.ts
+++
@@ -1,9 +0,0 @@
-import { isValidKorean } from "./inputValidation"
-
-export const stringFormat = (str: string) => {
-    const keyword = str.replaceAll(' ', '')
-    return (
-        isValidKorean(keyword) ? keyword : keyword.toUpperCase()
-    )
-
-}
